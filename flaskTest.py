ccf2f102cf37e49608e5817ccfc17ae29f7d3431
JohanOlsson99 <51018314+JohanOlsson99@users.noreply.github.com>
2020-11-06 17:03:23 +0100

    upload

--- flaskTest.py
+++ flaskTest.py
@@ -7,21 +7,21 @@ app = Flask(__name__)
 @app.route("/home")
 
 def homello():
-    dropdown_list = ['Air', 'Land', 'Sea']
+    #dropdown_list = ['Air', 'Land', 'Sea']
 
-    return render_template('home.html',dropdown_list=dropdown_list)
+    return render_template('home.html')#,dropdown_list=dropdown_list)
 
 @app.route("/about")
 def about():
-    return render_template('about.html', title=about)
+    return render_template('about.html', title="about")
 
 @app.route("/login")
 def login():
-    return render_template('login.html', title=about)
+    return render_template('login.html', title="login")
 
 @app.route("/register")
 def register():
-    return render_template('register.html', title=about)
+    return render_template('register.html', title="register")
 
 if __name__ == '__main__':
     app.run(debug=True)
