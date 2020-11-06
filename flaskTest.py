79a6888d1b7a94a75322ae8289b793b4cdfa5dae
philipbjornerud <philip.bjornerud@arcticgroup.se>
2020-11-06 11:12:26 +0100

    test

---
+++ flaskTest.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+
+@app.route("/")
+@app.route("/home")
+
+def homello():
+    dropdown_list = ['Air', 'Land', 'Sea']
+
+    return render_template('home.html',dropdown_list=dropdown_list)
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title=about)
+
+@app.route("/login")
+def login():
+    return render_template('login.html', title=about)
+
+@app.route("/register")
+def register():
+    return render_template('register.html', title=about)
+
+if __name__ == '__main__':
+    app.run(debug=True)
