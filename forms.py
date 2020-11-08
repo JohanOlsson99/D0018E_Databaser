45beab8cf36ed5557d28c3ba7b1b5b1b22d10033
dentro-8 <dentro-8@student.ltu.se>
2020-11-08 20:10:27 +0100

    Update forms.py

--- forms.py
+++ forms.py
@@ -3,17 +3,33 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Användarnamn',
-                           validators=[DataRequired(), Length(min=2, max=20)])
-    email = StringField('Email',
-                        validators=[DataRequired(), Email()])
-    password = PasswordField('Lösenord', validators=[DataRequired()])
+    username = StringField(
+        'Användarnamn',
+        validators=[DataRequired(), Length(min=2, max=20)]
+    )
+    email = StringField(
+        'Email',
+        validators=[DataRequired(), Email()]
+    )
+    password = PasswordField(
+        'Lösenord', 
+        validators=[DataRequired()]
+    )
 
     submit = SubmitField('Registrera dig')
 
 class LoginForm(FlaskForm):
-    email = StringField('Email',
-                        validators=[DataRequired(), Email()])
-    password = PasswordField('Lösenord', validators=[DataRequired()])
-    remember = BooleanField('Kom ihåg mig')
-    submit = SubmitField('Logga in')
+    email = StringField(
+        'Email', 
+        validators=[DataRequired(), Email()]
+    )
+    password = PasswordField(
+        'Lösenord', 
+        validators=[DataRequired()]
+    )
+    remember = BooleanField(
+        'Kom ihåg mig'
+    )
+    submit = SubmitField(
+        'Logga in'
+    )
