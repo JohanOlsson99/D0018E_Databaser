60f3c4bd7967c70c4a0fae6b17551a0fd421f913
JohanOlsson99 <51018314+JohanOlsson99@users.noreply.github.com>
2020-11-16 14:25:41 +0100

    Fixed one page for each item

--- Run.py
+++ Run.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template, flash, request, url_for, redirect
 from forms import RegistrationForm, LoginForm, PaymentForm, AddToCart, cartForm
-from addToCartForm import checkIfAddedToCart, getTest, getTestCart
-
+from addToCartForm import checkIfAddedToCart, getTest, getTestCart, checkIfAddedToCartItem
 import sys
+import random
+
 
 app = Flask(__name__, static_url_path='/static')
 
@@ -15,11 +16,14 @@ def home():
     form, id, description, imageLink = getTest(30)  # get test data
     checkIfAddedToCart(form, id)  # if the form was send and is correct
 
+    price = []
+
     for i in range(len(form)):
         form[i].howManyToCart.id = 'counter-display-' + str(
             id[i])  # IMPORTANT!!!! The + and - buttons won't work if this isn't here
+        price.append(random.randint(1,9999))
 
-    return render_template('home.html', title="home", form=form, id=id, description=description, imageLink=imageLink)
+    return render_template('home.html', title="home", form=form, id=id, description=description, imageLink=imageLink, price=price)
 
 
 @app.route("/varukorg", methods=['GET', 'POST'])
@@ -68,6 +72,21 @@ def betalning():
 @app.route("/com")
 def comment():
     return render_template('commentSection.html', title='comment')
+
+
+@app.route('/item-<int:id>', methods=['GET', 'POST'])
+def item(id):
+    form, idTest, description, imageLink = getTest(1)  # get test data
+    id = [id]
+    checkIfAddedToCartItem(form[0], id[0])  # if the form was send and is correct
+
+    form[0].howManyToCart.id = 'counter-display-' + str(
+        id[0])  # IMPORTANT!!!! The + and - buttons won't work if this isn't here
+    price = []
+    price.append(random.randint(1, 9999))
+
+    return render_template('item.html', title='item', form=form, id=id, description=description, imageLink=imageLink, price=price)
+
 
 
 if __name__ == '__main__':
--- addToCartForm.py
+++ addToCartForm.py
@@ -53,3 +53,18 @@ def getTestCart(length):
         startValue.append(random.randint(0, 100))
 
     return form, id, description, imageLink, startValue
+
+
+def checkIfAddedToCartItem(form, id):
+
+    if id in request.form:
+        print(id, file=sys.stderr)
+    if ((form.validate_on_submit()) and (form.addToCart.data == True) and (form.howManyToCart.data != 0)
+            and (id in request.form)):
+        print(form.howManyToCart.id, file=sys.stderr)
+        flash('Successfully added ' + str(form.howManyToCart.data) + ' of your items to your cart with ID ' +
+              str(id), 'success')
+
+    elif (form.is_submitted() and (not form.validate_on_submit()) and (id in request.form)):
+        flash('Couldn\'t add your items to your cart. Did you know you can only add 1 to 100 items not more '
+              'or less with ID ' + str(id), 'danger')
