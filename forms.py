008fc45e1b1b7ffa7e6337a5fe68b492e0fd466e
Hugo Pettersson <hugpet-8@student.ltu.se>
2020-11-23 17:14:21 +0100

    added register page

--- forms.py
+++ forms.py
@@ -1,42 +1,74 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField, DateField, SelectField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, NumberRange
 from flask_login import UserMixin
 
-
-
 class RegistrationForm(FlaskForm):
-    username = StringField('Username',
+    username = StringField('Användarnamn',
                            validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-
-    submit = SubmitField('Register')
+    password = PasswordField('Lösenord', validators=[DataRequired()])
+    firstName = StringField('Förnamn', validators=[DataRequired(), Length(max=20)])
+    surName = StringField('Efternamn', validators=[DataRequired(), Length(max=20)])
+    phone = IntegerField('Telefonnummer', validators=[])
+    birthdayDay = SelectField(
+        'Dag',
+        choices=[(1, '01'),(2, '02'),(3, '03'),(4, '04'),(5, '05'),(6, '06'),(7, '07'),(8, '08'),(9, '09'),(10, '10'),
+        (11, '11'),(12, '12'),(13, '13'),(14, '14'),(15, '15'),(16, '16'),(17, '17'),(18, '18'),(19, '19'),(20, '20'),(21, '21'),
+        (22, '22'),(23, '23'),(24, '24'),(25, '25'),(26, '26'),(27, '27'),(28, '28'),(29, '29'),(30, '30'),(31, '31')]
+    )
+    birthdayMonth = SelectField(
+        'Månad',
+        choices=[(1, '01'),(2, '02'),(3, '03'),(4, '04'),(5, '05'),(6, '06'),(7, '07'),(8, '08'),(9, '09'),(10, '10'),
+        (11, '11'),(12, '12')]
+    )
+    birthdayYear = SelectField(
+        'År',
+        choices=[(2020, '2020'),(2019, '2019'),(2018, '2018'),(2017, '2017'),(2016, '2016'),(2015, '2015'),
+        (2014, '2014'),(2013, '2013'),(2012, '2012'),(2011, '2011'),(2010, '2010'),(2009, '2009'),(2008, '2008'),
+        (2007, '2007'),(2006, '2006'),(2005, '2005'),(2004, '2004'),(2003, '2003'),(2002, '2002'),(2001, '2001'),
+        (2000, '2000'),(1999, '1999'),(1998, '1998'),(1997, '1997'),(1996, '1996'),(1995, '1995'),(1994, '1994'),
+        (1993, '1993'),(1992, '1992'),(1991, '1991'),(1990, '1990'),(1989, '1989'),(1988, '1988'),(1987, '1987'),
+        (1986, '1986'),(1985, '1985'),(1984, '1984'),(1983, '1983'),(1982, '1982'),(1981, '1981'),(1980, '1980'),
+        (1979, '1979'),(1978, '1978'),(1977, '1977'),(1976, '1976'),(1975, '1975'),(1974, '1974'),(1973, '1973'),
+        (1972, '1972'),(1971, '1971'),(1970, '1970'),(1969, '1969'),(1968, '1968'),(1967, '1967'),(1966, '1966'),
+        (1965, '1965'),(1964, '1964'),(1963, '1963'),(1962, '1962'),(1961, '1961'),(1960, '1960'),(1959, '1959'),
+        (1958, '1958'),(1957, '1957'),(1956, '1956'),(1955, '1955'),(1954, '1954'),(1953, '1953'),(1952, '1952'),
+        (1951, '1951'),(1950, '1950'),(1949, '1949'),(1948, '1948'),(1947, '1947'),(1946, '1946'),(1945, '1945'),
+        (1944, '1944'),(1943, '1943'),(1942, '1942'),(1941, '1941'),(1940, '1940'),(1939, '1939'),(1938, '1938'),
+        (1937, '1937'),(1936, '1936'),(1935, '1935'),(1934, '1934'),(1933, '1933'),(1932, '1932'),(1931, '1931'),
+        (1930, '1930'),(1929, '1929'),(1928, '1928'),(1927, '1927'),(1926, '1926'),(1925, '1925'),(1924, '1924'),
+        (1923, '1923'),(1922, '1922'),(1921, '1921'),(1920, '1920'),(1919, '1919'),(1918, '1918'),(1917, '1917'),
+        (1916, '1916'),(1915, '1915'),(1914, '1914'),(1913, '1913'),(1912, '1912'),(1911, '1911'),(1910, '1910'),
+        (1909, '1909'),(1908, '1908'),(1907, '1907'),(1906, '1906'),(1905, '1905'),(1904, '1904'),(1903, '1903'),
+        (1902, '1902'),(1901, '1901'),(1900, '1900')]
+    )
+    submit = SubmitField('Registrera dig')
 
 
 class LoginForm(FlaskForm):
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember = BooleanField('Remember')
-    submit = SubmitField('Login')
+    password = PasswordField('Lösenord', validators=[DataRequired()])
+    remember = BooleanField('Kom ihåg mig')
+    submit = SubmitField('Logga in')
 
 
 class PaymentForm(FlaskForm):
-    name = StringField('Full Name*', validators=[DataRequired()])
+    name = StringField('Fullständigt namn*', validators=[DataRequired()])
 
-    cardnumber = StringField('Card', validators=[DataRequired()])
+    cardnumber = StringField('Kort', validators=[DataRequired()])
 
     adress = StringField('Adress', validators=[DataRequired()])
 
-    Postnummer = StringField('Postal Number*', validators=[DataRequired()])
+    Postnummer = StringField('Postnummer*', validators=[DataRequired()])
 
-    Stad = StringField('City', validators=[DataRequired()])
+    Stad = StringField('Stad', validators=[DataRequired()])
 
-    Land = StringField('Country', validators=[DataRequired()])
+    Land = StringField('Land', validators=[DataRequired()])
 
-    submit = SubmitField('Send Order')
+    submit = SubmitField('Skicka bestälning')
 
 
 class AddToCart(FlaskForm):
@@ -48,13 +80,11 @@ class cartForm(FlaskForm):
     howManyToCart = IntegerField('amount', validators=[NumberRange(min=1, max=100)])
     addToCart = SubmitField('Remove')
 
-
 class User(UserMixin):
     id = 1
-    username = "Johan"
-    email = "Johan@gmail.com"
+    username = 'Johan'
+    email = 'johan@gmail.com'
     password = '123'
     isAdmin = True
-
     def getIsAdmin(self):
         return self.isAdmin
