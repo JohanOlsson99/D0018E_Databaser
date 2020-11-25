2ab0df4e140398682d8bf4d097ba069bebe00c0a
JohanOlsson99 <51018314+JohanOlsson99@users.noreply.github.com>
2020-11-25 12:43:36 +0100

    fixed login to the db

--- SendToDB.py
+++ SendToDB.py
@@ -2,6 +2,7 @@ from Run import MySQL
 from forms import *
 import sys
 
+
 def addCustomerInDB(form, con):
     try:
         cur = con.cursor()
@@ -14,13 +15,15 @@ def addCustomerInDB(form, con):
         else:
             highestID = 0
 
-        if str(form.birthdayYear.data) == '-' or str(form.birthdayMonth.data) == '-' or str(form.birthdayDay.data) == '-':
+        if str(form.birthdayYear.data) == '-' or str(form.birthdayMonth.data) == '-' or str(
+                form.birthdayDay.data) == '-':
             date = None
         else:
             date = str(form.birthdayYear.data) + '-' + str(form.birthdayMonth.data) + '-' + str(form.birthdayDay.data)
         cur.execute(
             "INSERT INTO Customer (Customer_ID, First_name, Last_name, Username, Email, Password, Phone_number, Birthday) VALUES (%s, %s, %s, %s, %s, %s, %s, %s);",
-            (highestID, form.firstName.data, form.surName.data, form.username.data, form.email.data, form.password.data, form.phone.data, date))
+            (highestID, form.firstName.data, form.surName.data, form.username.data, form.email.data, form.password.data,
+             form.phone.data, date))
         con.commit()
         cur.close()
         return True
@@ -47,29 +50,37 @@ def customerAlreadyInDB(form, con):
     except:
         return True
 
-def customerUsernameAndPasswordCorrect(form, con):
-    cur = con.cursor()
 
-    cur.execute("SELECT Password FROM Customer WHERE Username=%s", (form.email.data))
-    password = cur.fetchall()
-    cur.close()
-    if password != ():
-        password = password[0][0]
-    if password == form.password.data:
-        return True
-    else:
+def customerUsernameAndPasswordCorrect(form, con):
+    try:
+        cur = con.cursor()
+
+        cur.execute("SELECT Password FROM Customer WHERE Username=%s", (form.email.data))
+        password = cur.fetchall()
+        cur.close()
+        if password != ():
+            password = password[0][0]
+        if password == form.password.data:
+            return True
+        else:
+            return False
+    except:
         return False
+
 
 def customerEmailAndPasswordCorrect(form, con):
-    cur = con.cursor()
-    cur.execute("SELECT Password FROM Customer WHERE Email=%s", (form.email.data))
-    password = cur.fetchall()
-    cur.close()
-    if password != ():
-        password = password[0][0]
-    else:
-        return False
-    if password == form.password.data:
-        return True
-    else:
+    try:
+        cur = con.cursor()
+        cur.execute("SELECT Password FROM Customer WHERE Email=%s", (form.email.data))
+        password = cur.fetchall()
+        cur.close()
+        if password != ():
+            password = password[0][0]
+        else:
+            return False
+        if password == form.password.data:
+            return True
+        else:
+            return False
+    except:
         return False
